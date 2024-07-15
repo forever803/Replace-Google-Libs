24b718a59bdc9ff7be53a135c49584206d965209
forever803 <286856618@qq.com>
2024-07-15 16:30:47 +0800

    Update index.php
    Use USTC Open Libs Service to replace Google's.

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name: Replace Google Libs
- * Plugin URI:  http://www.soulteary.com/2014/06/15/replace-google-libs.html
- * Description: Use Qihoo 360 Open Libs Service to replace Google's.
- * Author:      soulteary
- * Author URI:  http://www.soulteary.com/
+ * Plugin URI:  https://github.com/forever803/Replace-Google-Libs
+ * Description: Use USTC Open Libs Service to replace Google's.
+ * Author:      xzhang
+ * Author URI:  https://github.com/forever803
  * Version:     1.0
  * License:     GPL
  */
@@ -23,7 +23,8 @@ class Replace_Google_Libs
      */
     public function __construct()
     {
-        add_filter('wp_print_scripts', array($this, 'ohMyScript'), 1000, 1);
+        // add_filter('wp_print_scripts', array($this, 'ohMyScript'), 1000, 1);
+        add_filter('wp_print_scripts', array($this, 'ustcReplaceScript'), 1000, 1);
     }
 
 
@@ -36,6 +37,30 @@ class Replace_Google_Libs
         if($wp_scripts && $wp_scripts->registered){
             foreach ($wp_scripts->registered as $libs){
                 $libs->src = str_replace('//ajax.googleapis.com', '//ajax.useso.com/', $libs->src);
+            }
+        }
+    }
+
+    /**
+     * Use USTC Open Libs Service to replace Google's.
+     */
+    public function ustcReplaceScript()
+    {
+		$replace_pairs = array(
+			'fonts.gstatic.com' => 'fonts-gstatic.lug.ustc.edu.cn',
+			'fonts.googleapis.com' => 'fonts.lug.ustc.edu.cn',
+			'ajax.googleapis.com' => 'ajax.lug.ustc.edu.cn',
+			'storage.googleapis.com ' => 'storage-googleapis.lug.ustc.edu.cn',
+			'themes.googleusercontent.com' => 'google-themes.lug.ustc.edu.cn',
+			'gerrit.googlesource.com' => 'gerrit-googlesource.lug.ustc.edu.cn',
+			'secure.gravatar.com' => 'gravatar.lug.ustc.edu.cn'
+		);
+        global $wp_scripts;
+        if($wp_scripts && $wp_scripts->registered){
+            foreach ($wp_scripts->registered as $libs){
+				foreach ($replace_pairs as $search => $replace) {
+					$libs->src = str_replace($search, $replace, $libs->src);
+				}
             }
         }
     }
